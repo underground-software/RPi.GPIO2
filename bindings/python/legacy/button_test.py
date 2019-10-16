5f50a94082c02fd2751d500ae41551a79b445b36
fabrizio8 <rpi@gselap-loaner1.fs.uml.edu>
2019-10-16 17:02:49 -0400

    help

--- bindings/python/legacy/button_test.py
+++ bindings/python/legacy/button_test.py
@@ -3,15 +3,19 @@
 import GPIO
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(19, GPIO.IN)
-GPIO.output(25, GPIO.HIGH)
+#GPIO.setup(25, GPIO.OUT)
+#GPIO.setup(19, GPIO.IN)
+
+#why doesn't it get seeet
+for i in range(1,26):
+    GPIO.setup(i,GPIO.IN)
+#GPIO.output(25, GPIO.HIGH)
 while True:
     
     input_val = GPIO.input(19)
     if not input_val:
         print("button pressed")
-        GPIO.output(25,GPIO.LOW)
+        #GPIO.output(25,GPIO.LOW)
         while not input_val:
             input_val = GPIO.input(19)
-        GPIO.output(25,GPIO.HIGH)
+        #GPIO.output(25,GPIO.HIGH)
