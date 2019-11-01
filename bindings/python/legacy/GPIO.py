df3ff9579fc9ea5a2f295cbe43df355edea5df64
fabrizio8 <fabrizio_dangelo@student.uml.edu>
2019-11-01 09:32:23 -0400

    rev3 lookup table

--- bindings/python/legacy/GPIO.py
+++ bindings/python/legacy/GPIO.py
@@ -5,6 +5,12 @@ import sys
 from threading import Thread, Event
 # === User Facing Data ===
 
+
+pin_to_gpio_rev3 = [
+                    -1, -1, -1, 2, -1, 3, -1, 4, 14, -1, 15, 17, 18, 27, -1, 22, 23, -1, 24, 10,
+                    -1, 9, 25, 11, 8, -1, 7, -1, -1, 5, -1, 6, 12, 13, -1, 19, 16, 26, 20, -1, 21
+                   ]
+
 # Pin numbering modes
 UNKNOWN = 0
 BCM     = 1
