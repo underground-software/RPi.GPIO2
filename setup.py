21cf82c37abe623b8082fb4565b89723909bcd76
Joel <joelsavitz@gmail.com>
2020-06-19 01:44:30 -0400

    Update python packaging files and prepare to release an alpha (#35)
    LICENSE -> LICENSE.txt:
    	- Renamed due to recommendation of PyPA
    
    	RPi/GPIO/__init__.py:
    	- Update VERSION2 constant to python package release number
    
    	setup.cfg:
    	- Create file as per recommendation of PyPA
    
    	setup.py:
    	- Update version number to match GitHub scheme
    	- Update classifiers
    	- Add project URLs (Source, Bug Reports)
    
    	tests/test_gpio.py:
    	- Add test for canonical python verison number
    	  using our new RPi.GPIO.VERSION2 constant
    
    Signed-off-by: Joel Savitz <joelsavitz@gmail.com>

--- setup.py
+++ setup.py
@@ -1,23 +1,44 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="RPi.GPIO",
-    version="0.0.2",
-    author="Underground Software",
-    author_email="fedora-rpi@googlegroups.com",
+setup(
+    name="RPi.GPIO2",
+    version="0.3.0a1",
+
+    author="Joel Savitz",
+    author_email="joelsavitz@gmail.com",
+
     description="Compatibility layer between RPi.GPIO syntax and libgpiod semantics",
     long_description=long_description,
     long_description_content_type="text/markdown",
+
     url="https://github.com/underground-software/python3-libgpiod-rpi",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
+
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-        "Development Status :: 2 - Pre-Alpha"
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)"
-        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Home Automation",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: System :: Hardware",
     ],
-    python_requires='>=3.6',
+
+    keywords="Raspberry Pi GPIO libgpiod RPi.GPIO",
+
+    python_requires=">=3.7",
+
+    project_urls={
+        "Source":       "https://github.com/underground-software/python3-libgpiod-rpi",
+        "Bug Reports":  "https://github.com/underground-software/python3-libgpiod-rpi/issues",
+    },
 )
